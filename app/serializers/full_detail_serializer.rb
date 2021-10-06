846476eb1727fcb77f4544f6aebef4bc1645017a
Howard Kwon <howie.ykwon70@gmail.com>
2021-10-06 10:48:25 -0400

    Details working except appearance

---
+++ app/serializers/full_detail_serializer.rb
@@ -0,0 +1,10 @@
+class FullDetailSerializer < ActiveModel::Serializer
+    attributes :id, :alias, :name, :creator, :universe, :powers, :like_count
+    
+    # has_one :appearance
+    has_one :background
+    has_one :powerstat
+
+    has_many :comments
+end
+  
