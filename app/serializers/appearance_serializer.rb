27b91145b138eb73ac77919ee43ecfa77e516aa9
Howard Kwon <howie.ykwon70@gmail.com>
2021-10-06 11:24:07 -0400

    Fixed appearance

--- app/serializers/appearance_serializer.rb
+++ app/serializers/appearance_serializer.rb
@@ -1,4 +1,4 @@
 class AppearanceSerializer < ActiveModel::Serializer
-  attributes :id, :gender, :origin, :height, :weight, :eye_color, :hair_color
-  has_one :superhero
-end
+    attributes :id, :gender, :species, :height, :weight, :eye_color, :hair_color, :img_url, :ani
+    has_one :superhero
+  end
